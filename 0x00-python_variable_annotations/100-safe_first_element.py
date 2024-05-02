758e1e756a93b9e3786e050b9bdefa3d451f9240
SaraAliMohammed <sara.ali.mohammed213@gmail.com>
2024-05-02 09:26:47 +0000

    Edit 100-safe_first_element file

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -4,7 +4,7 @@ the correct duck-typed annotations. '''
 from typing import Sequence, Any, Union
 
 
-def safe_first_element(lst: Sequence[Any]) -> Union[Any, NoneType]:
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
     ''' Return the first element of a list or None if the list is empty. '''
     if lst:
         return lst[0]
