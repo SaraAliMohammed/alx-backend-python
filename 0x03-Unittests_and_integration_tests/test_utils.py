0a5f41566d5f4291294d197afa370261bbbf7037
SaraAliMohammed <sara.ali.mohammed213@gmail.com>
2024-05-26 01:25:57 -0700

    Task2. Mock HTTP calls

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -44,5 +44,23 @@ class TestAccessNestedMap(unittest.TestCase):
             access_nested_map(nested_map, path)
 
 
+class TestGetJson(unittest.TestCase):
+    """ TestGetJson Class """
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False}),
+    ])
+    def test_get_json(
+            self,
+            test_url: str,
+            test_payload: Dict,
+            ) -> None:
+        """ To test that utils.get_json returns the expected result. """
+        attrs = {'json.return_value': test_payload}
+        with patch("requests.get", return_value=Mock(**attrs)) as req_get:
+            self.assertEqual(get_json(test_url), test_payload)
+            req_get.assert_called_once_with(test_url)
+
+
 if __name__ == "__main__":
     unittest.main()
